53da45ce5d41a36f097aaf3dfb5fd671a2597828
v1sstar <erquimajuan@gmail.com>
2024-05-01 11:08:25 -0500

    cualquier mensaje que quiera escribir

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ print("bdrms:", bdrms)
 colonial_binary = 1 if colonial == "Sí" else 0
 valores=np.array([[lotsize,assess,colonial_binary,sqrft,bdrms]])
 p=Lr.predict([[lotsize,assess,colonial_binary,sqrft,bdrms]])
-st.write("El valor del inmueble es"+(p))
+st.write(st.write("El valor del inmueble es " + str(p)))
