b7db10bfb6de8cc888c2dcaa79a1571c1b1ddce6
v1sstar <erquimajuan@gmail.com>
2024-05-01 10:25:44 -0500

    cualquier mensaje que quiera escribir

--- app.py
+++ app.py
@@ -11,10 +11,13 @@ lotsize = st.slider("Tamaño del lote (lotsize)", float(data["lotsize"].min()), 
 assess = st.slider("Evaluación del vecindario (assess)", float(data["assess"].min()), float(data["assess"].max()), float(data["assess"].mean()))
 colonial = st.radio("¿Es colonial?", ("Sí", "No"))
 sqrft = st.slider("Pies cuadrados (sqrft)", float(data["sqrft"].min()), float(data["sqrft"].max()), float(data["sqrft"].mean()))
+bdrms = st.slider("Pies cuadrados (bdrms)", float(data["bdrms"].min()), float(data["bdrms"].max()), float(data["bdrms"].mean()))
+
 print("lotsize:", lotsize)
 print("assess:", assess)
 print("colonial_binary:", colonial)
 print("sqrft:", sqrft)
+print("bdrms:", bdrms)
 
 colonial_binary = 1 if colonial == "Sí" else 0
 prediction = gs_xgb.predict(np.array([[lotsize, assess, colonial_binary, sqrft]]))[0]
