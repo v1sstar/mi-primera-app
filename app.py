d45db906760f229cc6019acc6879554acf50cde5
v1sstar <erquimajuan@gmail.com>
2024-05-01 09:58:51 -0500

    cualquier mensaje que quiera escribir

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open("rmse.pickle", "rb") as f:
     xgb_rmse = pl.load(f)
 # Widgets para la entrada de datos
 lotsize = st.slider("Tamaño del lote (lotsize)", float(data["lotsize"].min()), float(data["lotsize"].max()), float(data["lotsize"].mean()))
-asses = st.slider("Evaluación del vecindario (asses)", float(data["asses"].min()), float(data["asses"].max()), float(data["asses"].mean()))
+assess = st.slider("Evaluación del vecindario (assess)", float(data["assess"].min()), float(data["assess"].max()), float(data["assess"].mean()))
 colonial = st.radio("¿Es colonial?", ("Sí", "No"))
 sqrft = st.slider("Pies cuadrados (sqrft)", float(data["sqrft"].min()), float(data["sqrft"].max()), float(data["sqrft"].mean()))
 
