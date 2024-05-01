d6b0d22c47fddbba9e5f8b81c63a1cca9d0b6efc
v1sstar <erquimajuan@gmail.com>
2024-05-01 10:03:57 -0500

    cualquier mensaje que quiera escribir

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pickle as pl
 import numpy as np
 st.title("Predicción del valor de un inmueble")
 data = pd.read_csv("hprice.csv")
-with open("rmse.pickle", "rb") as f:
-    gs_xgb = pl.load(f)
+with open("rmse.picle", "rb") as f:
+    model = pl.load(f)
 # Widgets para la entrada de datos
 lotsize = st.slider("Tamaño del lote (lotsize)", float(data["lotsize"].min()), float(data["lotsize"].max()), float(data["lotsize"].mean()))
 assess = st.slider("Evaluación del vecindario (assess)", float(data["assess"].min()), float(data["assess"].max()), float(data["assess"].mean()))
@@ -13,5 +13,5 @@ colonial = st.radio("¿Es colonial?", ("Sí", "No"))
 sqrft = st.slider("Pies cuadrados (sqrft)", float(data["sqrft"].min()), float(data["sqrft"].max()), float(data["sqrft"].mean()))
 
 colonial_binary = 1 if colonial == "Sí" else 0
-prediction = gs_xgb.predict(np.array([[lotsize, assess, colonial_binary, sqrft]]))[0]
+prediction = model.predict(np.array([[lotsize, assess, colonial_binary, sqrft]]))[0]
 st.write(f"El valor predicho del inmueble es: ${prediction:.2f}")
